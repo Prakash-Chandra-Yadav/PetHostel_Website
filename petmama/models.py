4e727528c7ecc9fdefd2fbdcb6472dda99b42222
Prakash Chandra <prakashworks98@gmail.com>
2026-07-28 12:12:04 +0545

    created the contact model

--- petmama/models.py
+++ petmama/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Contact(models.Model):
+    location = models.TextField(max_length=50)
+    phone_number = models.TextField(max_length=18)
+    email = models.EmailField()
--- petmama/urls.py
+++ petmama/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .views import HomeView
 
-urlpatterns = [path("", HomeView.as_view())]
+urlpatterns = [path("", HomeView.as_view(), name="home")]
