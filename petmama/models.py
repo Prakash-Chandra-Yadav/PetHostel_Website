fd060a517e1e8c0917b7addce139328ac7972c30
Prakash Chandra <prakashworks98@gmail.com>
2026-07-28 12:16:55 +0545

    made the model to return the string on the admin in the human redable format

--- petmama/models.py
+++ petmama/models.py
@@ -6,3 +6,6 @@ class Contact(models.Model):
     location = models.TextField(max_length=50)
     phone_number = models.TextField(max_length=18)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.location
