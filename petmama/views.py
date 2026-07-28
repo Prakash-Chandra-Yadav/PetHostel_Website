8232043c5311ed35b2906e105ea0bc2a7c361eb1
Prakash Chandra <prakashworks98@gmail.com>
2026-07-28 13:16:18 +0545

    added the conact page

--- petmama/views.py
+++ petmama/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Contact
 
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "home.html"
+
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
