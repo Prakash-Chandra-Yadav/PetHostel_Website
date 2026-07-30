dc7bad8867d665f7c5f86a657b066891595eb8d4
Prakash Chandra <prakashworks98@gmail.com>
2026-07-30 12:15:15 +0545

    created the model for the customer form

--- petmama/models.py
+++ petmama/models.py
@@ -9,3 +9,13 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.location
+
+
+class CustomerContact(models.Model):
+    name = models.TextField(max_length=30)
+    contact_email = models.EmailField()
+    pet_type = models.Choices("Dog", "cat")
+    message = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.name
