b192a4ff1d75ca8a6911e3087a0ea59339f53b0a
Prakash Chandra <prakashworks98@gmail.com>
2026-07-30 12:59:45 +0545

    fixed the code of the form

--- petmama/forms.py
+++ petmama/forms.py
@@ -3,6 +3,6 @@ from .models import CustomerContact
 
 
 class CustomerContactForm(forms.ModelForm):
-    class meta:
+    class Meta:
         model = CustomerContact
         fields = ["name", "contact_email", "pet_type", "message"]
