ab46fb3a7ed6378dc69dc2372e9599f6bd818b4f
Prakash Chandra <prakashworks98@gmail.com>
2026-07-30 12:49:55 +0545

    added the logics in the views for the firm

--- petmama/views.py
+++ petmama/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, CreateView
 from .models import Contact, CustomerContact
+from .forms import CustomerContactForm
+from django.contrib import messages
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -16,3 +19,9 @@ class ContactCreateView(CreateView):
     model = CustomerContact
     template_name = "contact.html"
     fields = ["name", "contact_email", "pet_type", "message"]
+    success_url = reverse_lazy("contact")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Thanks! we will be in touch soon")
+        return response
