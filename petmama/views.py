6137167aa16b1976a780c057607546add2ce8960
Prakash Chandra <prakashworks98@gmail.com>
2026-07-30 12:18:53 +0545

    created the contact view for the customer to contact through the contact page

--- petmama/views.py
+++ petmama/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from .models import Contact
+from django.views.generic import TemplateView, CreateView
+from .models import Contact, CustomerContact
 
 
 # Create your views here.
@@ -10,3 +10,9 @@ class HomeView(TemplateView):
 
 class ContactView(TemplateView):
     template_name = "contact.html"
+
+
+class ContactCreateView(CreateView):
+    model = CustomerContact
+    template_name = "contact.html"
+    fields = ["name", "contact_email", "pet_type", "message"]
