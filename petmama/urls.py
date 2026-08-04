39a0e90f544383478b35f0aa310dcc0a318f0319
Prakash Chandra <prakashworks98@gmail.com>
2026-08-04 13:19:01 +0545

    added the model in the table

--- petmama/urls.py
+++ petmama/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, ContactCreateView
+from .views import HomeView, ContactCreateView, ReservationCreateView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("contact/", ContactCreateView.as_view(), name="contact"),
+    path("appointment/", ReservationCreateView.as_view(), name="appointment"),
 ]
