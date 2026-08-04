043e120f44b9f972e1464c7c6e4b7f7e25658c86
Prakash Chandra <prakashworks98@gmail.com>
2026-08-04 12:39:56 +0545

    registeres the model in the admin page

--- petmama/admin.py
+++ petmama/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Contact, CustomerContact
+from .models import Contact, CustomerContact, Reservation
 
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(CustomerContact)
+admin.sire.register(Reservation)
